8c1d28b589873d82f09960496ab66c8bdab9ae7f
Akshat Agarwal <akshat_ag@yahoo.com>
2018-08-02 16:35:58 +0530

    my sample npm package

--- source/index.js
+++ source/index.js
@@ -2,3 +2,15 @@ export default function calculatePercentage(percent=100, amount) {
     const percentAmount = (percent/100) * amount;
     return percentAmount;
 }
+
+export class Foo {
+    state = {
+        test: 1
+    };
+
+    doSomething = () => {
+        const test = {
+            ...this.state
+        };
+    }
+}
